eafa8e8dda20e71d7afa17a9dfa8b05e1b5d555d
Eric Ferrer-Vaughn <eric@if-surpro4-02.corp.intentionalfutures.com>
2017-10-18 17:29:25 -0700

    Firebase & Firestore Added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,26 @@
 import { push } from 'redux-little-router';
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
 
 export const STORE_QUERY = 'STORE_QUERY';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBf3VR3LyyFarmxDWEtChtApxXRrNrm3xA',
+  authDomain: 'full-5962f.firebaseapp.com',
+  databaseURL: 'https://full-5962f.firebaseio.com',
+  projectId: 'full-5962f',
+  storageBucket: '',
+  messagingSenderId: '559653873505'
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const objRef = db.collection('objects');
+
+const newObj = objRef.doc('object1').set({
+  key1: 'string-value',
+  key2: 3
+});
 
 export function navigateAbout() {
   return (dispatch) => {
